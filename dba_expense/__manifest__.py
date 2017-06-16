6f369afd428ce6a90e8b01efec9db224a5397666
Anand Patel <flt.anandkumar@gmail.com>
2017-06-16 18:45:28 +0530

    [ADD] Added module for Expense module extension

---
+++ dba_expense/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "DBA Expense Customization",
+    "author": "HashMicro/ Kunal",
+    "version": "10.0.1",
+    "website": "www.hashmicro.com",
+    "category": "expense",
+    "depends": ['base','product','account','hr_expense','document'],
+    "data": [
+        'security/ir.model.access.csv',
+        'data/expense_item_data.xml',
+		'views/hr_expense_view.xml',
+		'views/expense_report_view.xml',
+    ],
+    'description': '''Expense Customization''',
+    'demo': [],
+    "installable": True,
+    "auto_install": False,
+}
