df7dd7231f35f0cb29e73739ec06179841723898
Amit Patel <amitkumarnpatel@gmail.com>
2017-06-19 18:43:18 +0530

    [IMP] Contract Management Changes & Expense Report Header & Footer

---
+++ dba_modifier_account_analytic_analysis/models/account_analytic_analysis.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+# Part of GYB IT Solutions.
+
+from odoo import api, fields, models, _
+from odoo.exceptions import UserError, RedirectWarning, ValidationError
+from datetime import datetime
+
+
+class account_analytic_account(models.Model):
+    _inherit = 'account.analytic.account'
+    
+    contract_category_id = fields.Many2one('contract.category', string='Category')
+    description = fields.Char('Description')
+
+    _sql_constraints = [
+        ('name_uniq', 'unique(name)', 'Project Code already exists !'),
+    ]
+
+
+class contract_category(models.Model):
+    _name = 'contract.category'
+    _description = 'Contract Category'
+    
+    name = fields.Char('Name', required=True)
+
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
