9603c4e5a7933b8c8a8298237f37fb27959e7f45
Anand Patel <flt.anandkumar@gmail.com>
2017-07-18 18:53:50 +0530

    [IMP]Done the changes as discussed on DBA group today. User who does not submitted the timesheet will get the notification on Friday and Monday user and MAnager both will get notification if user has not submitted thte timesheet in last week.

--- dba_timesheet/models/timesheet.py
+++ dba_timesheet/models/timesheet.py
@@ -15,31 +15,28 @@ class TimeSheetExt(models.Model):
     def cron_remind_timesheet_submission(self):
         user_obj = self.env['res.users']
         notification_obj = self.env['timesheet.notification.config']
-        users = user_obj.search([])
         notification = notification_obj.search([])
         day_index = datetime.today().weekday()
         days_list = list(calendar.day_name)
         weekday = days_list[day_index]
         admin = user_obj.browse(self._uid)
         if notification:
-            print "\n\nnotification",notification
             if weekday == str(notification.reminder_day):
-                for user in users:
+                timesheets_objs = self.search([('state','=','draft')])
+                timesheet_emps = [tobj.employee_id for tobj in timesheets_objs]
+                for emp in timesheet_emps:
                     mail_values = {
-                    
                         'subject':'Please Fill Timesheet Today',
                         'author_id':self._uid,
                         'email_from':admin.partner_id.email or '',
-                        'email_to':user.partner_id.email,
-                        'recipient_ids':user.partner_id,
+                        'email_to':emp.user_id.partner_id.email,
+                        'recipient_ids':emp.user_id.partner_id,
                         'reply_to':admin.partner_id.email,
-                        'body_html':notification.reminder_message,
-                    
+                        'body_html':str(notification.reminder_message)%(emp.name,emp.parent_id.name),
                     }
                     mail_sent = self.env['mail.mail'].create(mail_values).send()
             
             if weekday == str(notification.notify_day):
-            
                 date_to = (datetime.today().date() - relativedelta(days=1)).strftime("%Y-%m-%d")
                 date_to = datetime.strptime(date_to,"%Y-%m-%d")
                 
@@ -48,37 +45,41 @@ class TimeSheetExt(models.Model):
                 
                 date_from = str(date_from).split(' ')[0]
                 date_to = str(date_to).split(' ')[0]
-                
-                timesheets_objs = self.search([('date_from','=',date_from),('date_to','=',date_to)])
-                timesheet_users = [tobj.employee_id.user_id.id for tobj in timesheets_objs]
-                not_submit_ts_users = user_obj.search([('id','not in',timesheet_users)])
-                
-                for user in not_submit_ts_users:
-                    related_employee = self.env['hr.employee'].search([('user_id','=',user.id)])
-                    if len(related_employee):
-                        employee = related_employee[0]
-                        man_email = employee.parent_id and employee.parent_id.user_id and employee.parent_id.user_id.partner_id.email 
-                        mail_values = {
-                        
-                            'subject':'%s did not submit timesheet'%(user.name),
-                            'author_id':self._uid,
-                            'email_from':admin.partner_id.email or '',
-                            'email_to':user.partner_id.email,
-                            'email_cc':man_email,
-                            'recipient_ids':user.partner_id,
-                            'reply_to':admin.partner_id.email,
-                            'body_html':notification.notify_message
-                        
-                        }
-                        mail_sent = self.env['mail.mail'].create(mail_values).send()
+                timesheets_objs = self.search([('date_from','=',date_from),('date_to','=',date_to),('state','=','draft')])
+                timesheet_emps = [tobj.employee_id for tobj in timesheets_objs]
+                for emp in timesheet_emps:
+                    man_email = emp.parent_id and emp.parent_id.user_id and emp.parent_id.user_id.partner_id.email or ''
+                    mail_values = {
+                        'subject':'%s did not submit timesheet'%(emp.name),
+                        'author_id':self._uid,
+                        'email_from':admin.partner_id.email or '',
+                        'email_to':user.partner_id.email,
+                        'email_cc':man_email,
+                        'recipient_ids':emp.user_id.partner_id,
+                        'reply_to':admin.partner_id.email,
+                        'body_html':str(notification.notify_message)%(emp.name,emp.parent_id.name),
+                    }
+                    mail_sent = self.env['mail.mail'].create(mail_values).send()
         return True
         
 class TimesheetNotificationConfig(models.Model):
     _name = 'timesheet.notification.config'
     
+    def _get_reminder_msg(self):
+        msg = '''Dear %s,<br/>'''
+        msg += '''You forgot to submit your timesheet this week <br/>'''
+        msg += '''Regards,'''
+        return msg
+    def _get_notify_msg(self):
+        msg = '''Dear %s,<br/>'''
+        msg += '''This is second notify<br/>'''
+        msg += '''you forgot to submit your timesheet last week<br/>'''
+        msg += '''Regards,'''
+        return msg
+    
     reminder_day = fields.Char(string="Reminder Day",required=True)
     notify_day = fields.Char(string="Notificaton Day",required=True)
-    reminder_message = fields.Html(string="Reminder Message",required=True)
-    notify_message = fields.Html(string="Notification Message",required=True)
+    reminder_message = fields.Html(string="Reminder Message",required=True,default=_get_reminder_msg)
+    notify_message = fields.Html(string="Notification Message",required=True,default=_get_notify_msg)
     
     _rec_name = 'reminder_day'
