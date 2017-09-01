e4419eb17625dc3df1fe8a3f920fc1de73ba760b
Anand Patel <flt.anandkumar@gmail.com>
2017-09-01 14:48:25 +0530

    [ADD]Set multiple expense reports to 'Post Journal Entries' from the List view.

--- dba_expense/wizard/hr_expense_wizard.py
+++ dba_expense/wizard/hr_expense_wizard.py
@@ -10,5 +10,15 @@ class ExpenseApplyApproverWizard(models.TransientModel):
         expense_ids = self._context.get('active_ids')
         self.env['hr.expense'].browse(expense_ids).write({'manager_id':self.employee_id.id})
         return True
+
+class ExpensePostJournalEntryWizard(models.TransientModel):
+    _name = 'expense.pj.wizard'
+    
+    @api.multi
+    def postJournalEntries(self):
+        expense_sheet_ids = self.env.context.get('active_ids')
+        for sheet_obj in self.env['hr.expense.sheet'].browse(expense_sheet_ids):
+            sheet_obj.action_sheet_move_create()
+        return True
     
     
