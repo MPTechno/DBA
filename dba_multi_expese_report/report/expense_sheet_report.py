4d5a61dfec286620a511bed5afa2a34ed702cc29
Anand Patel <flt.anandkumar@gmail.com>
2017-09-11 11:24:10 +0530

    [IMP]Added months in the Expense report and correct the Empoyee fields value alignment.

--- dba_multi_expese_report/report/expense_sheet_report.py
+++ dba_multi_expese_report/report/expense_sheet_report.py
@@ -3,6 +3,7 @@
 
 import time
 from odoo import api, fields, models
+import calendar
 
 class expense_sheet_report_dba(models.AbstractModel):
     _name = 'report.dba_multi_expese_report.expense_sheet_report'
@@ -63,8 +64,20 @@ class expense_sheet_report_dba(models.AbstractModel):
                     result.append(str(line.cheque_no))
         result = str(result).replace("['",'').replace("[",'').replace("']",'').replace("]",'').replace("'",'')
         return result
-
-        
+    @api.multi
+    def _get_months(self,data):
+        months = list(calendar.month_name)
+        expense_objs = self.env['hr.expense.sheet'].browse(data.get('ids'))
+        res = ''
+        for expense_obj in expense_objs:
+            for line in expense_obj.expense_line_ids:
+                date_data = line.date.split('-')
+                index = int(date_data[1])
+                year = date_data[0]
+                res_value = months[index] + ' ' + year + ' '
+                if res_value not in res:
+                    res+=  res_value
+        return res
     @api.model
     def render_html(self, docids, data=None):
         docargs = {
@@ -79,6 +92,7 @@ class expense_sheet_report_dba(models.AbstractModel):
             'get_currency_symbol': self._get_currency_symbol,
             'get_pc': self._get_pc,
             'get_cheque_no': self._get_cheque_no,
+            'get_months':self._get_months,
         }
         return self.env['report'].render('dba_multi_expese_report.expense_sheet_report', docargs)
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
